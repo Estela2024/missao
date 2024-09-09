b2246201ef8a3115fdddde6c84bb01d925c3ab80
Estela2024 <160504064+Estela2024@users.noreply.github.com>
2024-09-09 10:47:55 -0300

    76

--- script.js
+++ script.js
@@ -6,83 +6,17 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "O que fazer quando  identificar que uma criança está sofrendo bullying?",
+        enunciado: "Pergunta 1",
         alternativas: [
-            {
-                texto:"Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
-                afirmação:"afirmação1" //inserir a afirmação
-            },
-            {
-                texto:"Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
-                afirmação:"afirmação1"
-            },
-           // "Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
-           // "Intervir e defender a vítima, conversar com o agressor."
-    
-        ],
-    },
-    {
-    
-        enunciado: 
-        "A prática regular  de um esporte melhora  o condicionamento físico e a socialização. Você pratica algum esporte regularmente?",
-        alternativas: [
-            {
-                texto:"Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
-                afirmação:"afirmação2"
-            },
-            {
-                texto:"Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
-                afirmação:"afirmação2"
-            },
-           // "Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
- 
-            //"O esporte é uma atividade competitiva que tem regras fixas e treinamentos e pratico regularmente."
+            "Alternativa 1",
+            "Alternativa 2"
         ]
     },
     {
-    
-        enunciado: "A energia solar é uma fonte de energia renovável e sustentável que é obtida a partir da luz do sol. Diante disso eu:",
+        enunciado: "Pergunta 2",
         alternativas: [
-            {
-                texto:"Investiria em energia solar", 
-                afirmação: "afirmação3"
-            },
-            {
-                texto:"Investiria em energia solar", 
-                afirmação: "afirmação3"
-            },
-           // "Investiria em energia solar", 
-           // "Iria dar continuade ao uso da energia hidrelétrica"
+            "Alternativa 1",
+            "Alternativa 2"
         ]
-    }  
-];
-
-let atual = 0;/*variável com posição zero(listas começam com posição zero),o valor mudará quando percorrermos a lista (aula4)*/ 
-let perguntaAtual;/*variável vazia, receberá o texto da pergunta. Essa variável também mudará com o tempo, recebendo o texto de diferentes perguntas.(aula4)*/ 
-let historiaFinal = "";
-
-function mostraPergunta(){
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
-}
-
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
     }
-}
-
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacao = opcaoSelecionada.afirmacoes;
-    historiaFinal = afirmacao;
-    atual++;
-    mostraPergunta();
-}
-
-
-mostraPergunta();
+]; 
